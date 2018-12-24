26244126976ec684e84a4c9f5de3810de6a4a579
clewang <clewang@163.com>
2018-12-24 15:38:20 +0800

    change curl to wget

--- Crawler/file_util.py
+++ Crawler/file_util.py
@@ -9,12 +9,12 @@ from useragents import agents
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'}
 def read_json(path):
-    with open(path) as json_file:
+    with open(path, encoding='utf-8') as json_file:
         data = json.load(json_file)
         return data
 
 def write_json(path,json_data):
-    with open(path, 'w') as file_object:
+    with open(path, 'w', encoding='utf-8') as file_object:
         json.dump(json_data, file_object)
 
 def save_lib(url,path):
@@ -23,20 +23,20 @@ def save_lib(url,path):
     # with open(path, "wb") as f:
     #     f.write(lib.content)
     # f.close()
-    cmd = "curl -o " + path + " " + url
+    cmd = "wget -O " + path + " " + url
     process = subprocess.Popen(cmd)
 
 def get_lib_name(path):
     return path.split("/")[-1]
 
 def read_file(path):
-    with open(path, "r") as f:
+    with open(path, "r", encoding='utf-8') as f:
         lines = f.readlines()
         for i in range(len(lines)):
             lines[i] = lines[i].strip('\n')
     return lines
 
 def append_file(file_path,content):
-    with open(file_path, "a") as f:
+    with open(file_path, "a", encoding='utf-8') as f:
         f.write(content + "\n")
     f.close()
